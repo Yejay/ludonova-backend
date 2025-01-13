49a4f07ab4e9c5e782821a52de85fcf3ac104f70
Yejay Demirkan <demirkan.yejay@gmail.com>
2025-01-13 17:49:26 +0100

    fix

--- src/main/java/com/bht/ludonova/config/WebConfig.java
+++ src/main/java/com/bht/ludonova/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.bht.ludonova.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
